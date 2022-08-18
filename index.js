55ffd91f37a589d148c42d29d6fc909376973055
rogeriofourdev <chatbot.ti.developer@gmail.com>
2022-08-18 20:57:20 -0300

    chore: add gitignore and editing index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 let http = require('http');
 let fs = require('fs');
+const { listenerCount } = require('process');
 http.createServer(function (req, res) {
 
     var name = require('url').parse(req.url, true).query.name;
-    if(name === undefined) name = 'wolrd';
+    if(name === undefined) name = ' Wolrd';
     
     if (name == 'burningbird') {
         var file = 'phonenix5a.png';
@@ -12,10 +13,18 @@ http.createServer(function (req, res) {
             if(err) {
                 console.log('err');
                 res.writeHead(200, {'content-type': 'text/plain'});
-                res.end('sorry, burningbird isn´t around right now \n');
+                res.end('sorry, burningbird around right now \n');
             } else {
-                           
+               var img = fs.readFileSync(file);
+               res.contentType = 'image/png';
+               res.contentLength = stat.size;
+               res.end( img, 'binsry');
             }
-        }) 
+        }); 
+    } else {
+        res.writeHead( 200, { 'content-type': 'text/plain'});
+        res.end( 'hello' + name + '\n');
     }
-})
+}).listen(8124);
+
+console.log( 'server runing at port 8124');
